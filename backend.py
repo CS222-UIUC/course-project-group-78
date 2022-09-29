e3a75465d78c811824063f95d94a102dd3303371
Relena Li <relenali@wirelessprv-10-194-57-21.near.illinois.edu>
2022-09-28 19:03:36 -0500

    skeleton code added

---
+++ backend.py
@@ -0,0 +1,35 @@
+# import all libraries that we need
+
+
+# figure out how to take mp3 file from front end
+
+# function that takes in 1 parameter (music mp3 file)
+def analyze_music():
+
+    '''
+    call helpers and stuff
+    :return: a file of our actual beatmap
+    '''
+
+
+# helpers
+
+def breakdown():
+    '''
+    a function that takes in an mp3 file and break it down
+    :return: an array of frequencies
+    '''
+
+def analyze():
+    '''
+    takes in an array of frequencies and analyze the music
+    :return: our map in some kind of data type - tbd
+    '''
+
+def convert():
+    '''
+    takes in our map and convert it into an actual file
+    :return: actual beatmap file
+    '''
+
+# figure out how to send file back to frontend
---
+++ frontend.py
@@ -0,0 +1,5 @@
+# need to figure out how to connect godot with front end
+
+# need something that takes in a mp3 file and send it to backend
+
+# need something that takes the beatmap file from backend
