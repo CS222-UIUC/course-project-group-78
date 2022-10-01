81f39974968c4853bcb3b3d2e3a6da907c66cd31
Relena <lzztiancai@gmail.com>
2022-09-30 21:42:47 -0500

    testing2

--- frontend.py
+++ frontend.py
@@ -3,3 +3,9 @@
 # need something that takes in a mp3 file and send it to backend
 
 # need something that takes the beatmap file from backend
+
+def read_file():
+    '''
+    testing
+    :return:
+    '''
