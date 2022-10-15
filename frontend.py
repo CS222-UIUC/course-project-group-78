f2f37526b61b8cf2107bc5670af10396e30010b9
Nisha <nishap4@illinois.edu>
2022-10-15 00:54:53 -0500

    Put in some of my Godot scripts and progress in frontend.py. Haven't figured out how to connect Godot and Git yet, so I put a status update in there as well.

--- frontend.py
+++ frontend.py
@@ -4,4 +4,22 @@
 
 # need something that takes the beatmap file from backend
 
-print("test 2.0")
+'''
+Couldn't figure out how to connect Godot and Git yet. Have a basic UI up and running with
+a main menu and gampelay scene. There are buttons you can click to switch between the two. 
+Inlcuded button scripts below, don't know how to turn the whole project to code yet, will 
+try to have that done by next week.
+
+---------------------
+extends Node2D
+
+func _on_start_button_pressed():
+	get_tree().change_scene("res://scenes/gameplay_scene/gameplay_scene.tscn")
+
+---------------------
+extends Node2D
+
+func _on_quit_button_pressed():
+	get_tree().change_scene("res://scenes/main_scene/main_scene.tscn")
+
+'''
