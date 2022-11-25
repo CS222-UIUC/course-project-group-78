ae1fd4ace8f4a71ddf51027b4e11dce3fdcb3dac
Relena <lzztiancai@gmail.com>
2022-11-25 12:14:05 -0600

    added mp3 example

--- backend.py
+++ backend.py
@@ -1,36 +1,68 @@
 # import all libraries that we need
-
+from random import sample
+import math
+import librosa
+import numpy as np
+import scipy as sp
 
 # figure out how to take mp3 file from front end
 
 # function that takes in 1 parameter (music mp3 file)
-def analyze_music():
+# def analyze_music():
+#
+#     '''
+#     call helpers and stuff
+#     :return: a file of our actual beatmap
+#     '''
 
-    '''
-    call helpers and stuff
-    :return: a file of our actual beatmap
-    '''
+def freq_to_tile(freqs):
+    min = 10000000
+    tiles = []
+    curr_note = 0
+    len = 0;
+    for i in range(len(freqs)):
+        if(abs(freqs[i]-curr_note) <= 2**(1/12.0) and i < len(freqs)-1):
+            len += 1;
+        else:
+            if(len < min):
+                min = len
+            if(freqs[i] < curr_note):
+                if(tiles[len(tiles)-1][0] == 1):
+                    tiles.append((4,len))
+                else:
+                    tiles.append((tiles[len(tiles)-1][0]-1, len))
+            else:
+                if(tiles[len(tiles)-1][0] == 4):
+                    tiles.append((1,len))
+                else:
+                    tiles.append((tiles[len(tiles)-1][0]+1, len))
+            curr_note = freqs[i]
+            len = 1;
+    return tiles
 
 
 # helpers
 
-def breakdown():
+def breakdown(file):
     '''
-    a function that takes in an mp3 file and break it down
+    a function that takes in a mp3 file and break it down
     :return: an array of frequencies
     '''
+    data, sample_rate = librosa.load(file, sr=None)
+    return data, sample_rate # returns NumPy array
 
-def analyze():
-    '''
-    takes in an array of frequencies and analyze the music
-    :return: our map in some kind of data type - tbd
-    '''
-
-def convert():
-    '''
-    takes in our map and convert it into an actual file
-    :return: actual beatmap file
-    '''
+# def analyze():
+#     '''
+#     takes in an array of frequencies and analyze the music
+#     :return: our map in some kind of data type - tbd
+#     '''
+#
+#
+# def convert():
+#     '''
+#     takes in our map and convert it into an actual file
+#     :return: actual beatmap file
+#     '''
 
 
 # figure out how to send file back to frontend
--- tests/test.py
+++ tests/test.py
@@ -9,59 +9,59 @@ import unittest
 from os import path
 from pydub import AudioSegment
 
-# assign files
-input_file = "hello.mp3"
-output_file = "result.wav"
-
-# convert mp3 file to wav file
-sound = AudioSegment.from_mp3(input_file)
-sound.export(output_file, format="wav")
-'''
-
-# wavelength, frequencies, amplitude
-
-class MyTestCase(unittest.TestCase):
-    '''
-    # suppose my wav to wavelength function is called wavelength and follows the same convention for frequencies and amplitude
-    or I could do something like wavelength_to_test, frequencies_to_test, amplitude_to_test = convert(mp3 file)
-    '''
-    def test_wavelength(self):
-        wavelength_answer = []
-        wavelength_to_test = wavelength(mp3 file)
-        self.assertEqual(wavelength_to_test, wavelength_answer)  # add assertion here
-
-    def test_frequencies(self):
-        frequencies_answer = []
-        frequencies_to_test = frequencies(mp3 file)
-        self.assertEqual(frequencies_to_test, frequencies_answer)  # add assertion here
-
-    def test_amplitude(self):
-        amplitude_answer = []
-        amplitude_to_test = amplitude(mp3 file)
-        self.assertEqual(amplitude_to_test, amplitude_answer)  # add assertion here
-    '''
-    based on how we implement the algorithm, testing blocks might be different
-    '''
-    def test_map_size(self):
-        map_size_answer = 0 '''an integer, represents how many tiles we have'''
-        map_size_to_test = generatemap(freq, wave, amp)
-        self.assertEqual(map_size_to_test, map_size_answer)  # add assertion here
-
-    def test_tile_length(self):
-        tile_length_answer = 0 '''a float, represents how long the tiles are'''
-        tile_length_to_test = generatetile(freq[i], wave[i], amp[i])
-        self.assertEqual(tile_length_to_test, tile_length_answer)  # add assertion here
-
-    def test_hit(self):
-        hit_answer = False '''a boolean, represents the scores'''
-        hit_to_test = '''check input? how to convert click to signals real time'''
-        '''or should we just try to see that on frontend'''
-        self.assertEqual(hit_to_test, hit_answer)  # add assertion here
-
-    def test_score(self):
-        score_answer = 0 '''an int, represents the scores'''
-        score_to_test = getscore()
-        self.assertEqual(score_to_test, score_answer)  # add assertion here
-
-if __name__ == '__main__':
-    unittest.main()
+# # assign files
+# input_file = "hello.mp3"
+# output_file = "result.wav"
+# 
+# # convert mp3 file to wav file
+# sound = AudioSegment.from_mp3(input_file)
+# sound.export(output_file, format="wav")
+# '''
+#
+# # wavelength, frequencies, amplitude
+#
+# class MyTestCase(unittest.TestCase):
+#     '''
+#     # suppose my wav to wavelength function is called wavelength and follows the same convention for frequencies and amplitude
+#     or I could do something like wavelength_to_test, frequencies_to_test, amplitude_to_test = convert(mp3 file)
+#     '''
+#     def test_wavelength(self):
+#         wavelength_answer = []
+#         wavelength_to_test = wavelength(mp3 file)
+#         self.assertEqual(wavelength_to_test, wavelength_answer)  # add assertion here
+#
+#     def test_frequencies(self):
+#         frequencies_answer = []
+#         frequencies_to_test = frequencies(mp3 file)
+#         self.assertEqual(frequencies_to_test, frequencies_answer)  # add assertion here
+#
+#     def test_amplitude(self):
+#         amplitude_answer = []
+#         amplitude_to_test = amplitude(mp3 file)
+#         self.assertEqual(amplitude_to_test, amplitude_answer)  # add assertion here
+#     '''
+#     based on how we implement the algorithm, testing blocks might be different
+#     '''
+#     def test_map_size(self):
+#         map_size_answer = 0 '''an integer, represents how many tiles we have'''
+#         map_size_to_test = generatemap(freq, wave, amp)
+#         self.assertEqual(map_size_to_test, map_size_answer)  # add assertion here
+#
+#     def test_tile_length(self):
+#         tile_length_answer = 0 '''a float, represents how long the tiles are'''
+#         tile_length_to_test = generatetile(freq[i], wave[i], amp[i])
+#         self.assertEqual(tile_length_to_test, tile_length_answer)  # add assertion here
+#
+#     def test_hit(self):
+#         hit_answer = False '''a boolean, represents the scores'''
+#         hit_to_test = '''check input? how to convert click to signals real time'''
+#         '''or should we just try to see that on frontend'''
+#         self.assertEqual(hit_to_test, hit_answer)  # add assertion here
+#
+#     def test_score(self):
+#         score_answer = 0 '''an int, represents the scores'''
+#         score_to_test = getscore()
+#         self.assertEqual(score_to_test, score_answer)  # add assertion here
+#
+# if __name__ == '__main__':
+#     unittest.main()
